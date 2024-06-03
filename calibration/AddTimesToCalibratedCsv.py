d10b33a61e28950c38d32d2e1dff56cdb15a9e02
Joao Simioni <jasimioni@gmail.com>
2024-06-03 02:08:47 +0000

    2024-06-02-02

--- calibration/AddTimesToCalibratedCsv.py
+++ calibration/AddTimesToCalibratedCsv.py
@@ -4,14 +4,14 @@ import pandas as pd
 import os
 import sys
 
-BASEDIR = '/home/jasimioni/ppgia/globecom2024/'
+BASEDIR = os.environ.get('BASEDIR') or '/home/jasimioni/ppgia/'
 
-calibrated = {
-    'alexnet': 'calibration/calibrated/AlexNetWithExits',
-    'mobilenet': 'calibration/calibrated/MobileNetWithExits',
+target = {
+    'alexnet': 'calibration/pre_calibration/AlexNetWithExits',
+    'mobilenet': 'calibration/pre_calibration/MobileNetWithExits',
 } 
 
-non_calibrated = {
+rpi_times = {
     'alexnet': 'evaluations/rpi_evals/AlexNet/AlexNetWithExits_epoch_19_90.1_91.1.pth',
     'mobilenet': 'evaluations/rpi_evals/MobileNet/MobileNetV2WithExits_epoch_19_89.7_90.9.pth',
 }
@@ -21,20 +21,20 @@ for network in ['alexnet', 'mobilenet']:
         month = f'{m+1:02d}'
         filename = f"2016_{month}.csv"
 
-        calibrated_filename = os.path.join(BASEDIR, calibrated[network], filename)
+        target_filename = os.path.join(BASEDIR, target[network], filename)
 
-        df_calibrated = pd.read_csv(calibrated_filename)
-        df_non_calibrated = pd.read_csv(os.path.join(BASEDIR, non_calibrated[network], f'short_{filename}'))
+        df_target = pd.read_csv(target_filename)
+        df_rpi_times = pd.read_csv(os.path.join(BASEDIR, rpi_times[network], f'short_{filename}'))
 
-        bb_time_exit_1 = df_non_calibrated['bb_time_exit_1'].mean()
-        exit_time_exit_1 = df_non_calibrated['exit_time_exit_1'].mean()
-        bb_time_exit_2 = df_non_calibrated['bb_time_exit_2'].mean()
-        exit_time_exit_2 = df_non_calibrated['exit_time_exit_2'].mean()
+        bb_time_exit_1 = df_rpi_times['bb_time_exit_1'].mean()
+        exit_time_exit_1 = df_rpi_times['exit_time_exit_1'].mean()
+        bb_time_exit_2 = df_rpi_times['bb_time_exit_2'].mean()
+        exit_time_exit_2 = df_rpi_times['exit_time_exit_2'].mean()
 
-        df_calibrated['bb_time_exit_1'] = bb_time_exit_1
-        df_calibrated['exit_time_exit_1'] = exit_time_exit_1
-        df_calibrated['bb_time_exit_2'] = bb_time_exit_2
-        df_calibrated['exit_time_exit_2'] = exit_time_exit_2
+        df_target['bb_time_exit_1'] = bb_time_exit_1
+        df_target['exit_time_exit_1'] = exit_time_exit_1
+        df_target['bb_time_exit_2'] = bb_time_exit_2
+        df_target['exit_time_exit_2'] = exit_time_exit_2
 
-        df_calibrated.to_csv(calibrated_filename, index=False)
+        df_target.to_csv(target_filename, index=False)
     
---
+++ calibration/pre_calibration/README.md
@@ -0,0 +1,2 @@
+Todos os arquivos desse diretorio foram processados em CUDA (inferência e nivel de confiança)
+Porém os tempos foram substituídos pelos tempos do RASPBERRY
